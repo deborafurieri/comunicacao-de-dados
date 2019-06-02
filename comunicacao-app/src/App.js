e75f18269f979d83acd7aaa2e590706b50d7fda0
Bitbucket <debora.castiglioni@meupag.com.br>
2019-06-02 19:12:21 -0300

    Inserindo chart e fazendo ajustes css

--- comunicacao-app/src/App.js
+++ comunicacao-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Row } from 'react-materialize';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -9,34 +8,35 @@ import TableCell from '@material-ui/core/TableCell';
 import socketIOClient from "socket.io-client";
 
 import TableData from './TableData'
+import Chart from './Chart'
 
 class App extends Component {
   // inicializando os estados 
-  state = {
-    data: {
-      slaveId: 1,
-      setPoint: null,
-      value: null,
-      createdAt: null,
-      updateAt: null,
-    },
-    intervalIsSet: false,
-    isLoading: false,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [
+        {
+          slaveId: 1,
+          setPoint: null,
+          value: null,
+        }
+      ],
+      setPoint: 24.5,
+      intervalIsSet: false,
+      value: 0,
+    };
+  }
 
   // quando o componente monta, ele da um fetch em todo data existente no db
   componentDidMount() {
-    this.getDataFromDb();
-    // if (!this.state.intervalIsSet) {
-    //   let interval = setInterval(this.getDataFromDb, 1000);
-    //   this.setState({ intervalIsSet: interval });
-    // }
-
-
-    const socket = socketIOClient("http://localhost:3001");
-    socket.on("test", data => console.log(data));
+    if (!this.state.intervalIsSet) {
+      let interval = setInterval(this.getData(), 1000);
+      this.setState({ intervalIsSet: interval });
+    }
   }
-
+  
   // mata o processo
   componentWillUnmount() {
     if (this.state.intervalIsSet) {
@@ -44,78 +44,87 @@ class App extends Component {
       this.setState({ intervalIsSet: null });
     }
   }
-
-  // metodo get que usa a api backend, fetch data da nossa data base
-  getDataFromDb = () => {
-    fetch("http://localhost:3001/api/getData")
-      .then(data => data.json())
-      .then(res => this.setState({ data: res.data, isLoading: false }))
-      .catch(error => console.error(error));
-  };
-
-  // metodo post que usa a api backend, att valor de setpoint
-  putDataToDB = () => {
-    let currentSetPoint = this.state.setPoint;
-
-    axios.post("http://localhost:3001/api/update", {
-      slaveId: 1,
-      setPoint: currentSetPoint,
+  
+  getData() {
+    const socket = socketIOClient('http://localhost:3001');
+    socket.on('test', data => {
+      let allData = JSON.parse(data);
+      let value = allData.value;
+      this.setState({ data: [allData], value: value });
     });
-  };
+  }
+
+  sendSetPoint(setPoint) {
+    const socket = socketIOClient('http://localhost:3001');
+    socket.emit('client_data', setPoint);
+    console.log('setpoint', setPoint)
+  }
 
   // front
   render() {
-    const dados = [
-      {
-        slaveId: 1,
-        setPoint: 24.5,
-        value: 0.112,
-      },
-      {
-        slaveId: 1,
-        setPoint: 24.5,
-        value: 0.113,
-      }
-    ];
     return (
       <div>
         <Row>
-          <div style={{ padding: "10px" }}>
-            <input
-              type="text"
-              onChange={e => this.setState({ setPoint: e.target.value })}
-              placeholder="Setpoint"
-              style={{ width: "200px" }}
-            />
-            <button onClick={() => this.putDataToDB(this.state.setPoint)}>
-              Editar
-            </button>
-          </div>
+          <h1 style={{ height: '40px', backgroundColor: '#FFF', textAlign: 'center' }}>Comunicação de Dados</h1>
+          <h2 style={{ textAlign: 'center' }}>Sistema de Comunicação real baseado no Padrão RS-485</h2>
+          <h3 style={{ textAlign: 'center' }}>Débora Furieri, José Martins, Roberto Vasconcellos</h3>
         </Row>
-        <Row>
-          <Paper  
-            style={{ 
-              width: "60%",
-              marginTop: 10,
-              overflowX: 'auto',
-            }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Server ID</TableCell>
-                  <TableCell align="right">SetPoint</TableCell>
-                  <TableCell align="right">Data</TableCell>
-                </TableRow>
-              </TableHead>
-              {dados.map((dado) => (
-                <TableData 
-                  slaveId={dado.slaveId}
-                  setPoint={dado.setPoint}
-                  value={dado.value}
-                />
-              ))}
-          </Table>
-        </Paper>
+        <Row style={{ paddingLeft: '60px' }}>
+          <Row>
+            <div style={{ padding: "10px" }}>
+              <p style={{ fontWeight: 'bold' }}>Altere o SetPoint</p>
+              <input
+                type="text"
+                onChange={e => this.setState({ setPoint: e.target.value })}
+                placeholder="Setpoint"
+                style={{ width: "100px", marginRight: '10px'}}
+              />
+              <button onClick={() => this.sendSetPoint(this.state.setPoint)}>
+                Enviar
+              </button>
+            </div>
+          </Row>
+          <Row>
+            <Paper  
+              style={{ 
+                width: "50%",
+                marginTop: 10,
+                overflowX: 'auto',
+                marginLeft: "10px",
+              }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{ fontWeight: 'bold' }} align="center">Server ID</TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }} align="center">SetPoint</TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }} align="center">Data</TableCell>
+                  </TableRow>
+                </TableHead>
+                  {this.state.data.map((dado) => (
+                    <TableData 
+                      //slaveId={dado.slaveId} // primeiro escravo
+                      slaveId={1}
+                      setPoint={dado.setPoint}
+                      value={dado.value}
+                    />
+                  ))}
+                  {this.state.data.map((dado) => (
+                    <TableData 
+                      // slaveId={dado.slaveId} // segundo escravo
+                      slaveId={2}
+                      setPoint={dado.setPoint}
+                      value={dado.value}
+                    />
+                  ))}
+            </Table>
+          </Paper>
+          </Row>
+          <Row>
+            <Chart 
+              valueFirstSlave={this.state.value}
+              valueSecondSlave={this.state.value}
+            />
+          </Row>
         </Row>
       </div>
     );
